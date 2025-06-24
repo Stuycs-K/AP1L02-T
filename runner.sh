604d6f81b66e0c3c6c866acfc0786011d9902a91
konstantinnovation <konstans@stuy.edu>
2025-06-24 12:40:36 -0400

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# HEADERS ###########################################################
 err=0
+chmod +x .badge.sh
+./.badge.sh
 trap 'rm -f *.class' EXIT
 GREEN="\033[0;32m"
 RED="\033[0;31m"
 RESET="\033[0m"
-
 
 
 #################FILES THAT NEED COMPILE - TRY TO COMPILE #############
